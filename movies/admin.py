8b4570d264becfa4f8d916e5132348af150ac6e5
PythonNurs <nursultankaragulov0220@gmail.com>
2020-04-20 16:01:51 +0600

    using 'ckeditor editor in django admin' panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import Category, Genre, Movie, MovieShort, Actor, Rating, RatingStar, Reviews
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label='Description', widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 @admin.register(Category)
@@ -35,6 +45,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ('draft',)
+    form = MovieAdminForm
     readonly_fields = ('get_image', )
     # fields = (('actors', 'directors', 'genres'),)
     fieldsets = (
