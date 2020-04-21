45659093e9f9bab79ae112890e91e12beb275f6e
PythonNurs <nursultankaragulov0220@gmail.com>
2020-04-21 16:43:46 +0600

    filter film with modul 'Q'

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,28 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
-from .models import Movie, Actor
+from .models import Movie, Actor, Genre
 from .forms import ReviewForm
 
 
-class MovieViews(ListView):
+class GenreYear:
+    """Genre and year inside movie"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MovieViews(GenreYear, ListView):
     """List of Film"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = 'movies/movie_list.html'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Detail of Movie"""
     model = Movie
     slug_field = 'url'
@@ -32,9 +42,18 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorDetailView(DetailView):
+class ActorDetailView(GenreYear, DetailView):
     """Detail of Actor"""
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = 'name'
 
+
+class FilterMovie(GenreYear, ListView):
+    """Filter Movie"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        return queryset
