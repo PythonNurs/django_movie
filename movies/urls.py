ca94a57115ae86a2a2cba0b62e657d67067ad791
PythonNurs <nursultankaragulov0220@gmail.com>
2020-04-22 15:28:04 +0600

    add filter with 'ajax' and 'hogan.js'

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MovieViews, MovieDetailView, AddReview, ActorDetailView, FilterMovie
+from .views import MovieViews, MovieDetailView, AddReview, ActorDetailView, FilterMovie, JsonFilterMoviesView
 
 urlpatterns = [
     path('', MovieViews.as_view(), name='movie_list'),
     path('filter/', FilterMovie.as_view(), name='filter_movie'),
+    path('json-filter/', JsonFilterMoviesView.as_view(), name='json_filter'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('review/<int:pk>/', AddReview.as_view(), name='add_review'),
     path('actor/<str:slug>/', ActorDetailView.as_view(), name='actors_detail'),
