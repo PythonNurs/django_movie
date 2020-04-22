72d71f3b9d125514e9f6ff7d71d30444ae936e69
PythonNurs <nursultankaragulov0220@gmail.com>
2020-04-22 18:10:56 +0600

    add ratings for movie

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import MovieViews, MovieDetailView, AddReview, ActorDetailView, FilterMovie, JsonFilterMoviesView
+from .views import MovieViews, MovieDetailView, AddReview, ActorDetailView, FilterMovie, JsonFilterMoviesView, \
+    AddRatingStar
 
 urlpatterns = [
     path('', MovieViews.as_view(), name='movie_list'),
     path('filter/', FilterMovie.as_view(), name='filter_movie'),
     path('json-filter/', JsonFilterMoviesView.as_view(), name='json_filter'),
+    path('add-rating/', AddRatingStar.as_view(), name='add_rating'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('review/<int:pk>/', AddReview.as_view(), name='add_review'),
     path('actor/<str:slug>/', ActorDetailView.as_view(), name='actors_detail'),
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -57,3 +57,17 @@ let html = '\
         </div>\
     </div>\
 {{/movies}}'
+
+
+const rating = document.querySelector('form[name=rating]')
+
+rating.addEventListener('change', function (e) {
+    let data = new FormData(this)
+    fetch(`${this.action}`, {
+        method: 'POST',
+        body: data
+    })
+        .then(response => alert('Рейтинг установлен'))
+        .catch(error => alert('Error'))
+
+});
